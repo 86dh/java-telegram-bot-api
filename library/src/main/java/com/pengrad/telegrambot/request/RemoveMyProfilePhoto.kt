7c18d17aa14da1143a2061aa6569853a51b16417
Mirco Ianese <mirco.ianese@gmail.com>
2026-02-09 17:38:10 +0100

    Added the methods setMyProfilePhoto and removeMyProfilePhoto, allowing bots to manage their profile picture

---
+++ library/src/main/java/com/pengrad/telegrambot/request/RemoveMyProfilePhoto.kt
@@ -0,0 +1,9 @@
+package com.pengrad.telegrambot.request
+
+import com.pengrad.telegrambot.response.BaseResponse
+
+class RemoveMyProfilePhoto (
+
+) : KBaseRequest<RemoveMyProfilePhoto, BaseResponse>(BaseResponse::class) {
+
+}
