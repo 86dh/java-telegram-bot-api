91f4ef5920a321a46060702a88aa7cc5edda47e8
Mirco Ianese <mirco.ianese@gmail.com>
2026-02-09 17:43:26 +0100

    Added the class VideoQuality and the field qualities to the class Video

--- library/src/main/java/com/pengrad/telegrambot/model/Video.kt
+++ library/src/main/java/com/pengrad/telegrambot/model/Video.kt
@@ -1,5 +1,6 @@
 package com.pengrad.telegrambot.model
 
+import com.pengrad.telegrambot.model.checklist.Checklist
 import com.pengrad.telegrambot.utility.kotlin.JavaInteger
 
 data class Video(
@@ -17,7 +18,9 @@ data class Video(
 
     @get:JvmName("fileName") val fileName: String? = null,
     @get:JvmName("mimeType") val mimeType: String? = null,
-    @get:JvmName("fileSize") val fileSize: Long? = null
+    @get:JvmName("fileSize") val fileSize: Long? = null,
+
+    @get:JvmName("qualities") val qualities: Array<VideoQuality>? = null
 ) {
 
     /**
@@ -34,5 +37,39 @@ data class Video(
      * Backwards compatibility for Java code because of Kotlin optimization
      */
     fun duration() = duration as JavaInteger
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Video) return false
+
+        return fileId == other.fileId &&
+                fileUniqueId == other.fileUniqueId &&
+                width == other.width &&
+                height == other.height &&
+                duration == other.duration &&
+                thumbnail == other.thumbnail &&
+                cover == other.cover &&
+                startTimestamp == other.startTimestamp &&
+                fileName == other.fileName &&
+                mimeType == other.mimeType &&
+                fileSize == other.fileSize &&
+                qualities contentEquals other.qualities
+    }
+
+    override fun hashCode(): Int {
+        var result = fileId.hashCode()
+        result = 31 * result + fileUniqueId.hashCode()
+        result = 31 * result + width.hashCode()
+        result = 31 * result + height.hashCode()
+        result = 31 * result + duration.hashCode()
+        result = 31 * result + (thumbnail?.hashCode() ?: 0)
+        result = 31 * result + (cover?.hashCode() ?: 0)
+        result = 31 * result + (startTimestamp?.hashCode() ?: 0)
+        result = 31 * result + (fileName?.hashCode() ?: 0)
+        result = 31 * result + (mimeType?.hashCode() ?: 0)
+        result = 31 * result + (fileSize?.hashCode() ?: 0)
+        result = 31 * result + (qualities?.contentHashCode() ?: 0)
+        return result
+    }
 
 }
